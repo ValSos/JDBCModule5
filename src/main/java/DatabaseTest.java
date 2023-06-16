ab2c443289e734e7a3f7ab0413acfe71451e0022
Valeriia Sosiedka <lera.dovgan.96@gmail.com>
2023-06-16 19:28:58 +0300

    com2

--- src/main/java/DatabaseTest.java
+++ src/main/java/DatabaseTest.java
@@ -8,22 +8,20 @@ import java.sql.SQLException;
 public class DatabaseTest {
     public static void main(String[] args) throws SQLException, FileNotFoundException {
 
-        PrintAllClients printAllClients = new PrintAllClients();
-        printAllClients.printAllClients();
-/*        DatabaseInitService databaseInitService = new DatabaseInitService();
-        databaseInitService.databaseInit();*/
+        DatabaseInitService databaseInitService = new DatabaseInitService();
+        databaseInitService.databaseInit();
 
-        //DatabasePopulateService databasePopulateService = new DatabasePopulateService();
-/*        databasePopulateService.insertWorkers();
-        databasePopulateService.insertClient();*/
-        //databasePopulateService.insertProject();
-/*        databasePopulateService.insertProjectWorker();
+        DatabasePopulateService databasePopulateService = new DatabasePopulateService();
+        databasePopulateService.insertWorkers();
+        databasePopulateService.insertClient();
+        databasePopulateService.insertProject();
+        databasePopulateService.insertProjectWorker();
 
         DatabaseQueryService databaseQueryService = new DatabaseQueryService();
         databaseQueryService.findMaxProjectsClient();
         databaseQueryService.findLongestProject();
         databaseQueryService.findYoungestAndEldestWorkers();
         databaseQueryService.findMaxSalaryWorker();
-        databaseQueryService.printProfectPrices();*/
+        databaseQueryService.printProfectPrices();
     }
 }
